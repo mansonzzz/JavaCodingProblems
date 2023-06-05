070bf5f26123ab825710d443d80df2bbdb84f712
tianyi.zhang <tianyi.zhang@linkflowtech.com>
2023-06-05 18:07:32 +0800

    Description:<feat> Immutable objects in a nutshell

---
+++ src/main/java/com/m/jcp/chapter_2/Immutable_objects_in_a_nutshell.java
@@ -0,0 +1,45 @@
+package com.m.jcp.chapter_2;
+
+import java.lang.reflect.Field;
+
+/**
+ * @author zhangtian1
+ */
+public class Immutable_objects_in_a_nutshell {
+
+}
+
+class StringClazz {
+    public static void main(String[] args) throws Exception {
+        jdk17();
+    }
+
+    private static void jdk8() throws Exception {
+        String s = "Hello World";
+        System.out.println("s = " + s);
+
+        Field valueFieldOfString = String.class.getDeclaredField("value");
+        valueFieldOfString.setAccessible(true);
+
+        char[] value = (char[]) valueFieldOfString.get(s);
+        value[5] = '_';
+        System.out.println("s = " + s);
+    }
+
+    /**
+     * 由于在JDK17中，String类的实现已经从char[]变成了byte[]，所以在JDK9及以后的版本中，需要使用byte[]来修改字符串
+     * JDK9以上模块不能使用反射去访问非公有的成员/成员方法以及构造方法，除非模块标识为opens去允许反射访问。
+     */
+    private static void jdk17() throws Exception {
+        String s = "Hello World";
+        System.out.println("s = " + s);
+
+        Field valueFieldOfString = String.class.getDeclaredField("value");
+        // 需要使用`--add-opens java.base/java.lang=ALL-UNNAMED`来开放模块
+        valueFieldOfString.setAccessible(true);
+
+        byte[] value = (byte[]) valueFieldOfString.get(s);
+        value[5] = '_';
+        System.out.println("s = " + s);
+    }
+}
--- src/main/java/com/m/jcp/chapter_2/_TOC.md
+++ src/main/java/com/m/jcp/chapter_2/_TOC.md
@@ -1,1 +1,2 @@
 1. [checking null references in functional style and imperative code](Checking_null_references_in_functional_style_and_imperative_code.java)
+2. [immutable objects in a nutshell](Immutable_objects_in_a_nutshell.java)
